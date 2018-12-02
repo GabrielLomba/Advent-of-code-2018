7d32549fa639f317d97ec903f57b7c8c0e1ece5f
Gabriel Lomba <gabriellomba@hotmail.com>
2018-12-02 21:35:19 -0200

    Day 2 solutions

---
+++ day2/solutionPart1.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+
+function calculateIdChecksum(input) {
+  let contains2Letters = 0;
+  let contains3Letters = 0;
+
+  for (const id of input) {
+    const letterFreq = new Map();
+
+    for (const letter of id) {
+      const previousFreq = letterFreq.get(letter) || 0;
+      letterFreq.set(letter, previousFreq + 1);
+    }
+
+    const frequencies = [...letterFreq.values()];
+
+    let found2 = false;
+    let found3 = false;
+    let i = 0;
+
+    while (i < frequencies.length && !(found2 && found3)) {
+      found2 = found2 || frequencies[i] == 2;
+      found3 = found3 || frequencies[i] == 3;
+      ++i;
+    }
+
+    if (found2) ++contains2Letters;
+    if (found3) ++contains3Letters;
+  }
+
+  return contains2Letters * contains3Letters;
+}
+
+const rawInput = fs.readFileSync(`${__dirname}/inputPart1.txt`);
+
+const parsedInput = rawInput.toString().split("\n");
+
+console.log(calculateIdChecksum(parsedInput));
---
+++ day2/solutionPart2.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+
+function findCommonsLettersBetweenCorrectBoxes(input) {
+  for (const id1 of input) {
+    for (const id2 of input) {
+      let diff = 0;
+      let diffIdx = 0;
+      let i = 0;
+
+      while (i < Math.min(id1.length, id2.length) && diff <= 1) {
+        if (id1[i] != id2[i]) {
+          ++diff;
+          diffIdx = i;
+        }
+
+        ++i;
+      }
+
+      if (diff == 1) {
+        return id1.slice(0, diffIdx) + id1.slice(diffIdx + 1);
+      }
+    }
+  }
+}
+
+const rawInput = fs.readFileSync(`${__dirname}/inputPart2.txt`);
+
+const parsedInput = rawInput.toString().split("\n");
+
+console.log(findCommonsLettersBetweenCorrectBoxes(parsedInput));
